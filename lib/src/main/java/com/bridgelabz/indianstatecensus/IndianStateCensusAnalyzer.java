f8859792a9e2bd62c0cc2a3eb0d47fa36bd83178
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-25 14:13:51 +0530

    Added : Method to throw file not found exception

--- lib/src/main/java/com/bridgelabz/indianstatecensus/IndianStateCensusAnalyzer.java
+++ lib/src/main/java/com/bridgelabz/indianstatecensus/IndianStateCensusAnalyzer.java
@@ -3,29 +3,34 @@ package com.bridgelabz.indianstatecensus;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.List;
 
+import com.bridgelabz.indianstatecensus.CustomExceptionService.ExceptionType;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class IndianStateCensusAnalyzer
 {
 	private static final String FILE_PATH = "e://JavaWorkspace//IndianStateCensusAnalyzer//lib//src//main//resources";
-	private static final String INDIAN_STATE_CENCUS = "/IndiaStateCensusData.csv";
 
-	public List<StateCences> readInIndiaStateCensusData() 
+	public List<StateCences> readInIndiaStateCensusData(String FileName) 
 	{
 		try 
 		{
-			Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH+INDIAN_STATE_CENCUS));   //reader to read contacts
+			Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH+FileName));   //reader to read contacts
 			CsvToBean<StateCences> csvToBean = new CsvToBeanBuilder<StateCences>(reader)
 					.withType(StateCences.class)
 					.withIgnoreLeadingWhiteSpace(true)
 					.build();
 			return csvToBean.parse();   //Converting them to list
 		}
-		catch (IOException e)
+		catch (NoSuchFileException e)
+		{
+			throw new CustomExceptionService(ExceptionType.FILE_NOT_FOUND,"File Not Found");
+		}
+		catch (IOException e) 
 		{
 			e.printStackTrace();
 		}
--- lib/src/test/java/com/bridgelabz/indianstatecensustest/IndianStateCensusTest.java
+++ lib/src/test/java/com/bridgelabz/indianstatecensustest/IndianStateCensusTest.java
@@ -6,16 +6,35 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.bridgelabz.indianstatecensus.CustomExceptionService;
 import com.bridgelabz.indianstatecensus.IndianStateCensusAnalyzer;
 import com.bridgelabz.indianstatecensus.StateCences;
+import com.bridgelabz.indianstatecensus.CustomExceptionService.ExceptionType;
 
 public class IndianStateCensusTest 
 {
 	@Test
 	public void givenReadDataFromIndianCensus_ShouldReturnCountDataPresentInFile() 
 	{
+		String fileName = "/IndiaStateCensusData.csv";
 		IndianStateCensusAnalyzer censusService = new IndianStateCensusAnalyzer();
-		List<StateCences> stateCencesList = censusService.readInIndiaStateCensusData();
+		List<StateCences> stateCencesList = censusService.readInIndiaStateCensusData(fileName);
 		assertEquals(29, stateCencesList.size());
+	}
+	
+	@Test
+	public void givenWrongFile_ShouldReturnFileNotFound()
+	{
+		String fileName = "/IndiaStateCensus.csv";
+		IndianStateCensusAnalyzer censusService = new IndianStateCensusAnalyzer();
+		try 
+		{	
+			List<StateCences> stateCencesList = censusService.readInIndiaStateCensusData(fileName);
+			assertEquals(29, stateCencesList.size());	
+		}
+		catch (CustomExceptionService e) 
+		{
+			assertEquals(ExceptionType.FILE_NOT_FOUND, e.type);
+		}
 	}
 }
