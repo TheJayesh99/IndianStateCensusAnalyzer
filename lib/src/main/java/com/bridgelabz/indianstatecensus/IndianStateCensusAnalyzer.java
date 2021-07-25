757245b93c7efd3336126a2508d38b4e786c30c9
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-25 12:25:50 +0530

    Added : Method to load state cences csv data

--- lib/src/main/java/com/bridgelabz/indianstatecensus/IndianStateCensusAnalyzer.java
+++ lib/src/main/java/com/bridgelabz/indianstatecensus/IndianStateCensusAnalyzer.java
@@ -1,6 +1,34 @@
 package com.bridgelabz.indianstatecensus;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
 public class IndianStateCensusAnalyzer
 {
+	private static final String FILE_PATH = "e://JavaWorkspace//IndianStateCensusAnalyzer//lib//src//main//resources";
+	private static final String INDIAN_STATE_CENCUS = "/IndiaStateCensusData.csv";
 
+	public List<StateCences> readInIndiaStateCensusData() 
+	{
+		try 
+		{
+			Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH+INDIAN_STATE_CENCUS));   //reader to read contacts
+			CsvToBean<StateCences> csvToBean = new CsvToBeanBuilder<StateCences>(reader)
+					.withType(StateCences.class)
+					.withIgnoreLeadingWhiteSpace(true)
+					.build();
+			return csvToBean.parse();   //Converting them to list
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
--- lib/src/test/java/com/bridgelabz/indianstatecensustest/IndianStateCensusTest.java
+++ lib/src/test/java/com/bridgelabz/indianstatecensustest/IndianStateCensusTest.java
@@ -1,6 +1,21 @@
 package com.bridgelabz.indianstatecensustest;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.bridgelabz.indianstatecensus.IndianStateCensusAnalyzer;
+import com.bridgelabz.indianstatecensus.StateCences;
+
 public class IndianStateCensusTest 
 {
-
+	@Test
+	public void givenReadDataFromIndianCensus_ShouldReturnCountDataPresentInFile() 
+	{
+		IndianStateCensusAnalyzer censusService = new IndianStateCensusAnalyzer();
+		List<StateCences> stateCencesList = censusService.readInIndiaStateCensusData();
+		assertEquals(29, stateCencesList.size());
+	}
 }
